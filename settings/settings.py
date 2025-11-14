0f031ab455a6bef489bf52eebb72a3243d663a74
Juan D. Medellin C. <medellincalderonjuandavid@gmail.com>
2025-11-14 04:35:03 -0500

    actualización para desplegar en Railway

--- settings/settings.py
+++ settings/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = [
     "127.0.0.1", 
     "0.0.0.0",
     "juandavidmc.pythonanywhere.com",
+    "analisissenalesdiscretas-production.up.railway.app",
 ]
 
 
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'settings.urls'
